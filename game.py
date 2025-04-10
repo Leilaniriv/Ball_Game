e8d2382cc9ea02304ecd93032abf5b8ecd1a89cb
mbrookfieldLuc <mbrookfield@luc.edu>
2025-04-10 15:40:06 -0500

    Fixed missing restart

--- game.py
+++ game.py
@@ -19,16 +19,18 @@ d = 0
 jump = 0
 point = 0
 end = 0
-
+game_speed = 15
 jump_height = 0
 space_pressed_time = None
 
 def reset():
-    global l1, l2, l3, l4, l5, c, u, d, jump, point
+    global l1, l2, l3, l4, l5, c, u, d, jump, point, game_speed, end
     jump = 0
     u = 0
     d = 0
     point = 0
+    end = 0
+    game_speed = 15
     l1 = [0, randdim()]
     l2 = [200, randdim()]
     l3 = [400, randdim()]
@@ -54,9 +56,34 @@ def destroy():
 
 def endfunct(w):
     w.create_text(350, 100, text='Game Over')
+    w.create_text(350, 50, text='Press R to restart')
+
+def on_space(event):
+    global space_pressed_time
+    if space_pressed_time is None:
+        space_pressed_time = time.time()
+
+def on_space_release(event):
+    global jump, u, d, c, space_pressed_time, jump_height
+    if space_pressed_time is not None:
+        held_time = time.time() - space_pressed_time
+        space_pressed_time = None
+        held_time = max(0.1, min(held_time, 1.0))
+        jump_height = int(held_time * 100)
+        if not jump and c[0] == 200:
+            jump = 1
+            u = 0
+            d = 0
+
+def restart(event):
+    global end
+    if end == 1:
+        reset()
+        end = 0
+        w.after(game_speed, display)
 
 def touch(c):
-    global l1, l2, l3, l4, l5, end
+    global end
     for l in [l1, l2, l3, l4, l5]:
         if 25 < l[0] < 75:
             for i in range(l[1][1]):
@@ -66,27 +93,13 @@ def touch(c):
                     break
 
 def logic():
-    global l1, l2, l3, l4, l5, c, u, d, jump, point, space_pressed_time, jump_height
-
+    global l1, l2, l3, l4, l5, c, u, d, jump, point, game_speed
     for l in [l1, l2, l3, l4, l5]:
         if l[0] <= 0:
             n = np.random.randint(30, 100)
             l[0], l[1] = 950 + n, randdim()
         else:
             l[0] -= 1
-
-    if keyboard.is_pressed(' '):
-        if space_pressed_time is None:
-            space_pressed_time = time.time()
-    elif space_pressed_time is not None:
-        held_time = time.time() - space_pressed_time
-        space_pressed_time = None
-        held_time = max(0.1, min(held_time, 1.0))
-        jump_height = int(held_time * 100)
-        if c[0] == 200:
-            jump = 1
-            u = 0
-            d = 0
 
     if jump == 1:
         if u < jump_height:
@@ -100,11 +113,13 @@ def logic():
 
     if any(l[0] == 50 for l in [l1, l2, l3, l4, l5]):
         point += 1
+        if point % 10 == 0 and game_speed > 0:
+            game_speed -= 2
 
     touch(c)
 
 def display():
-    global w, l1, l2, l3, l4, l5, c, point
+    global w, c, point
     canvas_width = 700
     canvas_height = 400
     w.delete("all")
@@ -112,23 +127,22 @@ def display():
     for l in [l1, l2, l3, l4, l5]:
         rect(l, w)
     circle(c, w)
-    w.create_text(40, 40, text=f'points ==> {point}')
+    w.create_text(50, 40, text=f'points ==> {point}')
     logic()
-    print(point)
-
     if end == 0:
-        w.after(30, display)
+        w.after(game_speed, display)
     elif end == 1:
         endfunct(w)
+    w.create_text(600, 40, text=f'speed: {game_speed}')
 
 def game():
     global w
     master = Tk()
-    canvas_width = 700
-    canvas_height = 400
-    w = Canvas(master, width=canvas_width, height=canvas_height)
+    w = Canvas(master, width=700, height=400)
+    master.bind('<space>', on_space)
+    master.bind('<KeyRelease-space>', on_space_release)
+    master.bind("r", restart)
     display()
-    w.after(100, display)
     w.pack()
     mainloop()
 
